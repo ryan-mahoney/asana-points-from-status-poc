0d32f38ed9c30f459b5863fb6d60baa21c16c275
Ryan Mahoney <ryan@virtuecenter.com>
2020-08-04 20:38:15 -0400

    functional PoC

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# asana-points-from-status-poc
-Extract points completed in Sprint from an Asana status update
+# Asana Points From Status POC
+Extract points completed in Sprint from an Asana status update.
+
+### Usage
+The command:
+```sh
+ASANA_PROJECT=xxx ASANA_TOKEN=yyy node ./asana-status.js
+```
+
+produces output like this:
+```sh
+2020-07-29	9
+2020-07-23	6
+2020-07-16	0
+2020-01-13	0
+```
+
+### Notes
+It assumes that you have a custom field named `Sprint points complete` for tracking points completed in a Sprint.
---
+++ asana-status.js
@@ -0,0 +1,38 @@
+const asana = require("asana");
+
+const ASANA_TOKEN = process.env.ASANA_TOKEN;
+const ASANA_PROJECT = process.env.ASANA_PROJECT;
+
+const client = asana.Client.create().useAccessToken(ASANA_TOKEN);
+
+const parseCompletedFromText = (text) => {
+  const lines = text.split("\n");
+  const pointLine = lines.indexOf("Sprint points complete") + 1;
+  if (pointLine === 0) return 0;
+  return lines[pointLine];
+};
+
+client.projectStatuses
+  .getProjectStatusesForProject(ASANA_PROJECT, {
+    param: "value",
+    param: "value",
+    opt_pretty: true,
+  })
+  .then(({ data }) => {
+    return Promise.all(
+      data.map(({ gid }) =>
+        client.projectStatuses.getProjectStatus(gid, {
+          param: "value",
+          param: "value",
+          opt_pretty: true,
+        })
+      )
+    );
+  })
+  .then((data) => {
+    data.forEach(({ created_at, text }) => {
+      console.log(
+        `${created_at.split("T")[0]}\t${parseCompletedFromText(text)}`
+      );
+    });
+  });
